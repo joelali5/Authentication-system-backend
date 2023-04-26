e58f6ea2afd88e2ad46f8ed649a7b9c6e0b69f12
Joel Aliyu <joelaliyu1@gmail.com>
2023-04-26 23:46:16 +0100

    Making changes for production

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: `${__dirname}/../.env.test` });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE or DATABASE_URL configured!");
 }
 
-const config = { connectionString: process.env.PGDATABASE };
+const config = { connectionString: process.env.DATABASE_URL, max: 2 };
 
 module.exports = new Pool(config);
