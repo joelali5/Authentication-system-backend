4467d651c83ede187526d6ddaa5719256c9c3ff4
Joel Aliyu <joelaliyu1@gmail.com>
2023-04-28 11:50:07 +0100

    removing the redirect

--- user.controller.js
+++ user.controller.js
@@ -37,7 +37,7 @@ exports.signup = async (req, res, next) => {
     //Create the new user
     const newUser = await createUser(email, hashedPassword);
     const user = { email: newUser.email };
-    return res.redirect("/sign");
+    res.status(200).send(user);
   } catch (error) {
     next(error);
   }
